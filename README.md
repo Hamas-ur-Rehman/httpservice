da1e095760bc8cf2f109227fc7bcd111487a6d1e
Hamas Ur Rehman <47780362+Hamas-ur-Rehman@users.noreply.github.com>
2021-07-31 19:29:48 +0500

    added new methods

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # httpservice
 
-A FLutter package that depends on http package (For now only get command is supported).
+A FLutter package that depends on http package.
+
 
 ## Getting Started
-
+ 
 This package contains classes that make it
 easy to consume the http package. It's multi-platform, and supports mobile, desktop,
 and the browser.
+It supports GET,PUT,POST,DELETE and PATCH requests
 
 ## Using
 
@@ -21,6 +23,75 @@ void getapidata(String apix) async {
     var data = await httpService.getContents();
     var decodedData = jsonDecode(data);
     print(decodedData);
+  }
+
+```
+
+# GET
+
+```dart
+import 'dart:convert';
+import 'package:httpservice/httpservice.dart';
+
+void getapidata(String apix) async {
+    HttpService httpService = HttpService("$apix");
+    var data = await httpService.getContents();
+    var decodedData = jsonDecode(data);
+    print(decodedData);
+  }
+
+```
+
+
+# POST
+
+```dart
+import 'dart:convert';
+import 'package:httpservice/httpservice.dart';
+
+void postapidata(String apix) async {
+    HttpService httpService = HttpService("$apix");
+    var data = await httpService.postContents(headers: {"Content-type": "application/json"},body: '{"title": "Hello", "body": "body text", "userId": 1}');
+  }
+
+```
+
+
+# PUT
+
+```dart
+import 'dart:convert';
+import 'package:httpservice/httpservice.dart';
+
+void postapidata(String apix) async {
+    HttpService httpService = HttpService("$apix");
+    var data = await httpService.putContents(headers: {"Content-type": "application/json"},body:'{"title": "Hello", "body": "body text", "userId": 1}');
+  }
+
+```
+
+# PATCH
+
+```dart
+import 'dart:convert';
+import 'package:httpservice/httpservice.dart';
+
+void postapidata(String apix) async {
+    HttpService httpService = HttpService("$apix");
+    var data = await httpService.putContents(headers: {"Content-type": "application/json"},body:'{"title": "Hello"}');
+  }
+
+```
+
+# DELETE
+
+```dart
+import 'dart:convert';
+import 'package:httpservice/httpservice.dart';
+
+void postapidata(String apix) async {
+    HttpService httpService = HttpService("$apix");
+    var data = await httpService.deleteContents();
   }
 
 ```
--- lib/httpservice.dart
+++ lib/httpservice.dart
@@ -7,6 +7,7 @@ class HttpService {
 
   HttpService(this.url);
 
+  //GET METHOD
   Future getContents() async {
     var urlx = Uri.parse(url);
     try {
@@ -15,6 +16,77 @@ class HttpService {
         print(response.statusCode);
         var data = response.body;
         return data;
+      } else {
+        print("Error Fetching DATA");
+      }
+    } catch (e) {
+      print("Error Fetching DATA");
+    }
+  }
+
+  //POST METHOD
+  Future<void> postContents(
+      {required Map<String, String> headers, required String body}) async {
+    var urlx = Uri.parse(url);
+    try {
+      var response = await http.post(urlx, headers: headers, body: body);
+      if (response.statusCode == 200) {
+        print(response.statusCode);
+        print('Body: ${response.body}');
+        print('Message: Successful');
+      } else {
+        print("Error Fetching DATA");
+      }
+    } catch (e) {
+      print("Error Fetching DATA");
+    }
+  }
+
+  //PUT METHOD
+  Future<void> putContents(
+      {required Map<String, String> headers, required String body}) async {
+    var urlx = Uri.parse(url);
+    try {
+      var response = await http.put(urlx, headers: headers, body: body);
+      if (response.statusCode == 200) {
+        print(response.statusCode);
+        print('Body: ${response.body}');
+        print('Message: Successful');
+      } else {
+        print("Error Fetching DATA");
+      }
+    } catch (e) {
+      print("Error Fetching DATA");
+    }
+  }
+
+  //PATCH METHOD
+  Future<void> patchContents(
+      {required Map<String, String> headers, required String body}) async {
+    var urlx = Uri.parse(url);
+    try {
+      var response = await http.patch(urlx, headers: headers, body: body);
+      if (response.statusCode == 200) {
+        print(response.statusCode);
+        print('Body: ${response.body}');
+        print('Message: Successful');
+      } else {
+        print("Error Fetching DATA");
+      }
+    } catch (e) {
+      print("Error Fetching DATA");
+    }
+  }
+
+  //DELETE METHOD
+  Future<void> deleteContents() async {
+    var urlx = Uri.parse(url);
+    try {
+      var response = await http.delete(urlx);
+      if (response.statusCode == 200) {
+        print(response.statusCode);
+        print('Body: ${response.body}');
+        print('Message: Successful');
       } else {
         print("Error Fetching DATA");
       }
